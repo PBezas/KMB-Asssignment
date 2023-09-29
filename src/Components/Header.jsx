adf840ec35af9150a5129dd2b5a8f50906acf59d
Panos Bezas <p.bezas1@gmail.com>
2023-09-29 13:02:40 +0300

    display user profile photo and name in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,15 @@
+import "../App.css";
+import { users } from "../data/users.jsx";
+
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header className="header">
+      {users.map((user) => (
+        <div key={user.id} className="userContainer">
+          <img src={user.img} alt={user.name} className="profileImg" />
+          <h2>{user.name}</h2>
+        </div>
+      ))}
+    </header>
+  );
 }
---
+++ src/data/users.jsx
@@ -0,0 +1,3 @@
+import profileImg from "../assets/images/profileImg.webp";
+
+export const users = [{ id: 1, name: "Panos Bezas", img: profileImg }];
