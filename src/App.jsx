ddc3ea4c9d6fb8a16254a47ae62bed212dd452ea
Panos Bezas <p.bezas1@gmail.com>
2023-09-29 12:43:58 +0300

    Install react-router-dom and create basic routing for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return;
-}
+import RootLayout from "./Routes/RootLayout";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+  },
+]);
+const App = () => <RouterProvider router={router} />;
 export default App;
---
+++ src/Routes/RootLayout.jsx
@@ -0,0 +1,13 @@
+import Header from "../Components/Header";
+import Main from "../Components/Main";
+import Footer from "../Components/Footer";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
