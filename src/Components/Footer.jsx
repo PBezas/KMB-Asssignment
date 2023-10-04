28bd29acf2afaba2975dc1d39868b6c76d83b4ac
Panos Bezas <p.bezas1@gmail.com>
2023-10-04 15:28:35 +0300

    feat: add footer content and style footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,31 @@
 export default function Footer() {
-  return <h1>Footer</h1>
+  return (
+    <footer className="appFooter">
+      <p>Copyright Creative Workers</p>
+
+      <ul>
+        <li>
+          <a href="http://google.com" target="_blank" rel="noreferrer">
+            Privacy
+          </a>
+        </li>
+        <li>
+          <a href="http://google.com" target="_blank" rel="noreferrer">
+            Terms
+          </a>
+        </li>
+        <li>
+          <a href="http://google.com" target="_blank" rel="noreferrer">
+            Cookies
+          </a>
+        </li>
+        <li>
+          <a href="http://google.com" target="_blank" rel="noreferrer">
+            More
+          </a>
+        </li>
+      </ul>
+      <p>Kissmybutton © 2017</p>
+    </footer>
+  );
 }
