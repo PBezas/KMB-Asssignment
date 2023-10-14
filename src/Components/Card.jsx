51ae5a2ccb81eccdb83cb9d6b2622dd1c4bebd8e
Panos Bezas <p.bezas1@gmail.com>
2023-10-14 16:08:56 +0300

    fix: display defaultImg in cards that img is null

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,13 @@
-
+import defaultImg from "../assets/images/default_article_image.webp";
 
 export default function Card({ urlToImage, title, content }) {
   return (
     <article className="article">
-      <img src={urlToImage} alt="article img" className="articleImg" />
+      <img
+        src={urlToImage ?? defaultImg}
+        alt="article img"
+        className="articleImg"
+      />
       <div className="articleBody">
         <h3>{title}</h3>
         <p>{content}</p>
